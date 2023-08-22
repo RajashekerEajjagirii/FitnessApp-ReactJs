7edd2f63b06b7bb3bd97968160affeedb410ad9f
Eajjagiri_Rajasheker <Eajjagiri_Rajasheker@steris.com>
2023-08-22 17:12:28 +0530

    Deploying Fitness App

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,17 +3,17 @@ export const exerciseOptions={
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
-      
+      // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+      'X-RapidAPI-Key': '537f9eac5cmsh6e54c0cc5c27f6fp1221c1jsn31061fb11d98'
     }
   };
 
 export const youtubeOptions={
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY ,
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY ,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    
+    'X-RapidAPI-Key': '537f9eac5cmsh6e54c0cc5c27f6fp1221c1jsn31061fb11d98'
   }
 };
 
