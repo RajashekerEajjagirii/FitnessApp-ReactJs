570eb9035a1cc4064b3daba89a34c92d9f942ef3
Eajjagiri_Rajasheker <Eajjagiri_Rajasheker@steris.com>
2023-08-22 23:56:59 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
     return (
         <Stack direction='row' justifyContent='space-around'
          sx={{gap:{sm:'122px',xs:'44px'},mt:{sm:'30px',xs:'20px',justifyContent:'none'}}} px='20px'>
-            <Link to='/home'>
+            <Link to='/'>
                 <img src={Logo} alt='logo-image' 
                 style={{width:'48px',height:'48px', margin:'0 20px'}} />
             </Link>
             <Stack direction="row" gap='40px' fontSize='24px' alignItems='flex-end'>
-                <Link to='/home' style={{textDecoration:'none',color:'#3A1212',
+                <Link to='/' style={{textDecoration:'none',color:'#3A1212',
                     borderBottom:'3px solid #FF2625'}}>Home
                 </Link>
                 <a href='#exercises' style={{textDecoration:'none',color:'#3A1212'}}>Exercises</a>
